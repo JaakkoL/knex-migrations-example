28c4a6697dd4a2b5ddc2c6cfeab7ae641f388f90
JaakkoL <jaakko.o.laurila@gmail.com>
2019-11-19 14:52:24 +0200

    Add knexfile.js.

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Separate config for different environments, if needed.
+// See: http://knexjs.org/#knexfile
+module.exports = {
+  local: {
+    client: 'mysql', // mariadb can be used with mysql driver
+    connection: process.env.DATABASE_URL || {
+      host : '127.0.0.1',
+      user : 'root',
+      password : 'secret',
+      database : 'migrations_example'
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+};
